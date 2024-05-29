006f8cc952e1a2da075c36929f53ffca085d7a19
Sandip  Pradhan <158505928+sandip9851@users.noreply.github.com>
2024-05-29 18:23:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-// your code here
 document.getElementById('button').addEventListener('click', function(event) {
   event.preventDefault();
   let name = document.querySelector("#name").value;
-	let age = document.querySelector("#year").value;
-  let qry = `https://localhost:8080/?name=${name}&year=${age}`
-  // update h3 text
-	let head = document.querySelector("#url");
-	head.innerText = qry;
+  let year = document.querySelector("#year").value; // corrected this line
+  let qry = `https://localhost:8080/?name=${name}&year=${year}`
+  let head = document.querySelector("#url");
+  head.innerText = qry;
 });
