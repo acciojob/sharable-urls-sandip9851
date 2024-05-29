4b6c719304c920e0a2811a1b9b8c56bbffa0ae06
Sandip  Pradhan <158505928+sandip9851@users.noreply.github.com>
2024-05-29 18:31:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById('button').addEventListener('click', function(event) {
   event.preventDefault();
   let name = document.querySelector("#name").value;
-  let year = document.querySelector("#year").value; // corrected this line
+  let year = document.querySelector("#year").value;
   let qry = `https://localhost:8080/?name=${name}&year=${year}`
   let head = document.querySelector("#url");
   head.innerText = qry;
